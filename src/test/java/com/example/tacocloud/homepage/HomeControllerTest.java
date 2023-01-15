5dd446539535a6fe6ff7320cde02da0445e9a845
Click <raschmichael10@gmail.com>
2023-01-14 23:04:37 -0600

    Added designing taco page and order form

---
+++ src/test/java/com/example/tacocloud/homepage/HomeControllerTest.java
@@ -0,0 +1,27 @@
+package com.example.tacocloud.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest()
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("home"))
+                .andExpect(MockMvcResultMatchers.content().string(containsString("Welcome to....")));
+    }
+
+}
